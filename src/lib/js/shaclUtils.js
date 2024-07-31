bbd1decd0c673056fe285af07d55464363dc0427
wcrd <11898748+wcrd@users.noreply.github.com>
2024-07-30 21:51:56 -0600

    Add deno jupyter kernel and create working document. Get import and graph functions working, ready for dev

--- src/lib/js/shaclUtils.js
+++ src/lib/js/shaclUtils.js
@@ -3,11 +3,11 @@ import N3 from 'n3'
 const { namedNode } = N3.DataFactory
 
 /// This is a dev function. Will make more abstract in future
-function getAndEnrichTopLevelResults(graphstore){
+function getAndEnrichTopLevelResults(graphstore) {
     const topLevelResults = getResultNodes(graphstore)
 
     const data = []
-    for (let res of topLevelResults){
+    for (let res of topLevelResults) {
         // get all pred-objs
         const quads = graphstore.getQuads(res, null, null, null)
 
@@ -16,7 +16,7 @@ function getAndEnrichTopLevelResults(graphstore){
         const keys = quads.map(r => r._predicate.id.split("#")[1])
         const values = quads.map(r => r._object.id)
 
-        data.push({id: res.id, ...createObject(keys, values)})
+        data.push({ id: res.id, ...createObject(keys, values) })
     }
 
     return data
@@ -33,7 +33,7 @@ function createObject(keys, values) {
     const obj = Object.fromEntries(
         keys.map((key, index) => [key, values[index]]),
     );
- 
+
     return obj;
 }
 
